2455946a45de2fae05b1905c690992a80d3c7535
flobo593 <git@floribo.de>
2021-01-08 22:25:39 +0100

    Misc Doc

---
+++ misc/head.php
@@ -0,0 +1,26 @@
+<?php
+include 'https://localhost/poll/publishcms/sql.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="/styles/main.css" />
+  <meta charset="utf-8" />
+  <meta legal="<?php $getcopyright; ?>. Useable under the Licence: <?php $getlicence; ?>" />
+  <meta name="author" http-equiv="<?php $getauthor; ?>" />
+  <meta name="publisher" content="<?php $getpublisher; ?>" />
+  <meta name="description" content="<?php $getdescription; ?>" />
+  <meta name="keywords" content="<?php $getkeywords; ?>" />
+  <meta name="page-topic" content="<?php $getpagetopic; ?>" />
+  <meta name="page-type" content="<?php $getpagetype; ?>" />
+  <meta name="audience" content="<?php $getaudience; ?>" />
+  <meta http-equiv="content-language" content="<?php $getlanguage; ?>" />
+  <meta name="robots" content="<?php $getrobotinfo; ?>" />
+  <meta name="DC.Creator" content="<?php $getauthor; ?>" />
+  <meta name="DC.Publisher" content="<?php $getpublisher; ?>" />
+  <meta name="DC.Rights" content="<?php $getcopyright; ?>. Useable under the Licence: <?php $getlicence; ?>" />
+  <meta name="DC.Description" content="<?php $getdescription; ?>" />
+  <meta name="DC.Language" content="<?php $getlanguage; ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title><?php $gettitle; ?></title>
+</head>
