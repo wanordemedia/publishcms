637a83165bb462b6f9f38ed511832946a05df14b
flobo593 <git@floribo.de>
2021-01-09 10:42:54 +0100

    GetMeta-Script
    Created and filled

---
+++ misc/meta.php
@@ -0,0 +1,30 @@
+<?php
+//defines all the $get's
+$getauthor = mysqli_query("SELECT author FROM content WHERE permalink = $getpermalink");
+$getpublisher = mysqli_query("SELECT content  FROM globalsettings WHERE name = 'publisher'");
+$getdescription = mysqli_query("SELECT description FROM content WHERE permalink = $getpermalink");
+$getkeywords = mysqli_query("SELECT keywords FROM content WHERE permalink = $getpermalink");
+$getpagetopic = mysqli_query("SELECT pagetopic FROM content WHERE permalink = $getpermalink");
+$getpagetype = mysqli_query("SELECT pagetype FROM content WHERE permalink = $getpermalink");
+$getaudience = mysqli_query("SELECT content FROM globalsettings WHERE name = 'audience'");
+$getlanguage = mysqli_query("SELECT content FROM globalsettings WHERE name = 'language'");
+$getcopyright = mysqli_query("SELECT copyright FROM content WHERE permalink = $getpermalink");
+$gettitle = mysqli_query("SELECT title FROM content WHERE permalink = $getpermalink");
+$getheader = mysqli_query("SELECT header FROM content WHERE permalink = $getpermalink");
+
+$follow = mysqli_query("SELECT follow FROM content WHERE permalink = $getpermalink");
+$doindex = mysqli_query("SELECT doindex FROM content WHERE permalink = $getpermalink");
+if ($follow) {
+  $getrobotinfo = "follow, ";
+}
+else {
+  $getrobotinfo = "nofollow, ";
+}
+
+if ($doindex) {
+  $getrobotinfo .= "index";
+}
+else {
+  $getrobotinfo .= "noindex";
+}
+?>
