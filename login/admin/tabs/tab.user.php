c3e5796f41dea5995f2902387f2a29a05df22317
Joshua <mintech10@gmail.com>
2021-01-28 21:34:26 +0100

    User-Tab
    - The function of user management was added

--- login/admin/tabs/tab.user.php
+++ login/admin/tabs/tab.user.php
@@ -1,5 +1,35 @@
-<?php 
-/* Include the Settings
-*/
-  include "../misc/sql.php";
+<?php
+
+$db = new mysqli($serverpath,$db_user,$db_pw,$db_name);
+
+if($db->connect_error){
+  die("Verbindung fehlgeschlagen: " . $db->connect_error);
+}
+
+$sql = "SELECT * FROM $user_table";
+$ergebnis = $db->query("SET NAMES 'utf8'");
+$ergebnis = $db->query($sql);
+
+if($ergebnis->num_rows >0){
+
+  echo "<table><tr><th>Name</th><th>Deine Lizenz</th><th>URL</th><th>Text</th><th>Bearbeiten</th></tr>";
+
+  while($row = $ergebnis->fetch_assoc()){
+   $id = $row['ID'];
+   $username = $row['username'];
+   $shown_name = $row['shown_name'];
+   $email = $row['email'];
+   $text = $row['text'];
+
+   echo "<tr><td>".$username."</td><td>".$shown_name."</td><td>".$email."</td><td><a href='daten-aendern.php?edit=$id'>Bearbeiten</a></td></tr>";
+
+  }
+
+  echo "</table>";
+
+}else{
+  echo "Leider nichts gefunden";
+}
+
+$db->close();
 ?>
