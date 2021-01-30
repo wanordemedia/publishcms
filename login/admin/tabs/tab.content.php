224c6545d52d37bbe51224e7d99a404d358922cb
Joshua <mintech10@gmail.com>
2021-01-30 20:32:47 +0100

    Content + htaccess
    - The folder with the files for the individual tabs was secured with the .htaccess.
    - The display in the admin panel for the content was added

--- login/admin/tabs/tab.content.php
+++ login/admin/tabs/tab.content.php
@@ -1,5 +1,34 @@
 <?php
-/* Include the Settings
-*/
-  include "../misc/sql.php";
+
+$db = new mysqli($serverpath,$db_user,$db_pw,$db_name);
+
+if($db->connect_error){
+  die("Verbindung fehlgeschlagen: " . $db->connect_error);
+}
+
+$sql = "SELECT * FROM $content_table";
+$ergebnis = $db->query("SET NAMES 'utf8'");
+$ergebnis = $db->query($sql);
+
+if($ergebnis->num_rows >0){
+
+  echo "<table><tr><th>Titel</th><th>Autoren</th><th>Datum</th><th>Einstellungen</th></tr>";
+
+  while($row = $ergebnis->fetch_assoc()){
+   $id = $row['ID'];
+   $titel = $row['title'];
+   $author = $row['author'];
+   $creadate = $row['crea-date'];
+
+   echo "<tr><td>".$titel."</td><td>".$author."</td><td>".$creadate."</td><td><a href='edit/edit.content.php?edit=$id'>Bearbeiten</a></td></tr>";
+
+  }
+
+  echo "</table>";
+
+}else{
+  echo "Leider nichts gefunden";
+}
+
+$db->close();
 ?>
